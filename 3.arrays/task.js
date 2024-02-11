b629977c10a5a03b7acfabba18b9854484a2ae72
Roman <borisenko0893@gmail.com>
2024-02-11 18:50:53 +0300

    Добавлено ДЗ по Объектам

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -6,12 +6,16 @@ function compareArrays(arr1, arr2) {
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-	const filteredUsers = users.filter(user => user.gender === gender);
+	/* const filteredUsers = users.filter(user => user.gender === gender);
 	if (filteredUsers.length === 0) {
 		return 0;
 	}
 	const ages = filteredUsers.map(user => user.age);
 	const sumOfAges = ages.reduce((total, age) => total + age, 0);
 	const averageAge = sumOfAges / ages.length;
-	return averageAge;
+	return averageAge; */
+
+	return users.filter(user => user.gender === gender)
+    .map(user => user.age)
+    .reduce((acc, age, index, arr) => acc + age / arr.length, 0);
 }
